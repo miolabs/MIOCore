bfd7dc02f671cb61547dac117f62b111c47c78cf
Javier Segura <javier@miolabs.com>
2020-06-01 00:58:33 +0200

    Adding the missing implementations

---
+++ Sources/MIOCore/NSExpression+Linux.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Javier Segura Perez on 01/06/2020.
+//
+
+import Foundation
+
+#if os(Linux)
+
+extension NSExpression
+{
+    open var keyPath: String { return "HI" }
+}
+
+#endif
