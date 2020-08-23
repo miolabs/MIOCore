31c00b8b8e21a7349aa60286dba3c38a03db3c6e
Javier Segura <javier@miolabs.com>
2020-08-23 13:10:39 +0200

    Adding XMLSerialization

---
+++ Sources/MIOCore/XMLSerialization.swift
@@ -0,0 +1,80 @@
+//
+//  XMLSerialization.swift
+//  
+//
+//  Created by Javier Segura Perez on 23/08/2020.
+//
+
+import Foundation
+
+
+public class XMLSerialization:NSObject, XMLParserDelegate
+{
+    static public func xmlObject(with data:Data, options:[Any]) throws -> Any
+    {
+        let xs = XMLSerialization(with: data)
+        xs.parse()
+        return xs.results!
+    }
+        
+    let dataContents:Data
+    init(with data:Data){
+        dataContents = data
+    }
+    
+    func parse() {
+        let parser = XMLParser(data: dataContents)
+        parser.delegate = self
+        parser.parse()
+    }
+    
+    public var results:Any?
+    
+    var elementStack:[Any] = []
+    var currentElement:[String:Any]?
+    
+    var foundCharacters:String = ""
+    
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        
+        if currentElement != nil { elementStack.append(currentElement!) }
+        
+        currentElement = [:]
+        currentElement!["__XML_TAG_NAME__"] = elementName
+        
+        foundCharacters = ""
+    }
+    
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+                
+        if currentElement != nil {
+
+            if (currentElement!["__XML_TAG_NAME__"] as! String) == elementName {
+                if foundCharacters.count > 0 {
+                    var prevElement = elementStack.last as! [String:Any]
+                    prevElement[elementName] = foundCharacters
+                    elementStack[elementStack.count - 1] = prevElement
+                }
+            }
+        }
+        else {
+            currentElement = elementStack.popLast() as? [String:Any]
+            var prevElement = elementStack.last as? [String:Any]
+            if prevElement != nil {
+                prevElement![elementName] = currentElement
+                elementStack[elementStack.count - 1] = prevElement!
+            }
+            else {
+                results = currentElement
+            }
+        }
+        
+        foundCharacters = ""
+        currentElement = nil
+        //elementStack.popLast()
+    }
+    
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        foundCharacters += string
+    }
+}
--- Tests/MIOCoreTests/MIOCoreTests.swift
+++ Tests/MIOCoreTests/MIOCoreTests.swift
@@ -2,14 +2,53 @@ import XCTest
 @testable import MIOCore
 
 final class MIOCoreTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(MIOCore().text, "Hello, World!")
+   
+    func testXMLSerialize() throws {
+        let xml =
+        """
+        <?xml version=\'1.0\' encoding=\'UTF-8\'?>
+            <MaterialDocumentResponse>
+                <Status>Created</Status>
+                <body>
+                    <CreationDate>2020-08-21T00:00:00.000</CreationDate>
+                    <MaterialDocumentYear>2020</MaterialDocumentYear>
+                    <MaterialDocument>4900647222</MaterialDocument>
+                    <CreatedByUser>CC0000000001</CreatedByUser>
+                    <VersionForPrintingSlip>2</VersionForPrintingSlip>
+                    <GoodsMovementCode>04</GoodsMovementCode>
+                    <MaterialDocumentHeaderText>IO/135</MaterialDocumentHeaderText>
+                    <ReferenceDocument/>
+                    <InventoryTransactionType>WA</InventoryTransactionType>
+                    <PostingDate>2020-08-20T00:00:00.000</PostingDate>
+                    <to_MaterialDocumentItem/>
+                    <ManualPrintIsTriggered/>
+                    <CreationTime>1970-01-01T15:13:17.000</CreationTime>
+                    <DocumentDate>2020-08-20T00:00:00.000</DocumentDate>
+                </body>
+        </MaterialDocumentResponse>
+        """
+        let data = xml.data(using: .utf8)!
+        let xmlDict = try XMLSerialization.xmlObject(with: data, options: []) as! [String:Any]
+        
+        XCTAssertTrue( xmlDict.count == 3, "3" )
+        XCTAssertTrue( (xmlDict["__XML_TAG_NAME__"] as! String) == "MaterialDocumentResponse", "MaterialDocumentResponse" )
+        XCTAssertTrue( (xmlDict["Status"] as! String) == "Created", "MaterialDocumentResponse['Status'] != Created" )
+        let body = xmlDict["body"] as? [String:Any]
+        XCTAssertTrue( body != nil, "MaterialDocumentResponse['body'] is null" )
+        XCTAssertTrue( (body!["__XML_TAG_NAME__"] as! String) == "body", "body" )
+        XCTAssertTrue( (body!["CreationDate"] as! String) == "2020-08-21T00:00:00.000", "MaterialDocumentResponse['body']['CreationDate'] != 2020-08-21T00:00:00.000" )
+        XCTAssertTrue( (body!["MaterialDocumentYear"] as! String) == "2020", "MaterialDocumentResponse['body']['MaterialDocumentYear'] != 2020" )
+        XCTAssertTrue( (body!["MaterialDocument"] as! String) == "4900647222", "MaterialDocumentResponse['body']['MaterialDocument'] != 4900647222" )
+        XCTAssertTrue( (body!["CreatedByUser"] as! String) == "CC0000000001", "MaterialDocumentResponse['body']['CreatedByUser'] != CC0000000001" )
+        XCTAssertTrue( (body!["VersionForPrintingSlip"] as! String) == "2", "MaterialDocumentResponse['body']['VersionForPrintingSlip'] != 2" )
+        XCTAssertTrue( (body!["GoodsMovementCode"] as! String) == "04", "MaterialDocumentResponse['body']['GoodsMovementCode'] != 04" )
+        XCTAssertTrue( (body!["MaterialDocumentHeaderText"] as! String) == "IO/135", "MaterialDocumentResponse['body']['MaterialDocumentHeaderText'] != IO/135" )
+        XCTAssertTrue( body!["ReferenceDocument"] == nil, "MaterialDocumentResponse['body']['ReferenceDocument'] != nil" )
+        XCTAssertTrue( (body!["InventoryTransactionType"] as! String) == "WA", "MaterialDocumentResponse['body']['InventoryTransactionType'] != WA" )
+        XCTAssertTrue( (body!["PostingDate"] as! String) == "2020-08-20T00:00:00.000", "MaterialDocumentResponse['body']['PostingDate'] != 2020-08-20T00:00:00.000" )
+        XCTAssertTrue( body!["to_MaterialDocumentItem"] == nil, "MaterialDocumentResponse['body']['to_MaterialDocumentItem'] != nil" )
+        XCTAssertTrue( body!["ManualPrintIsTriggered"] == nil, "MaterialDocumentResponse['body']['ManualPrintIsTriggered'] != nil" )
+        XCTAssertTrue( (body!["CreationTime"] as! String) == "1970-01-01T15:13:17.000", "MaterialDocumentResponse['body']['CreationTime'] != 1970-01-01T15:13:17.000" )
+        XCTAssertTrue( (body!["DocumentDate"] as! String) == "2020-08-20T00:00:00.000", "MaterialDocumentResponse['body']['DocumentDate'] != 2020-08-20T00:00:00.000" )
     }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
 }
