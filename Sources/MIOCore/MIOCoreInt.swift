ae5a2e7372e671e7b56eb5c6dc3ee3d68ca4864f
Javier Segura <javier@miolabs.com>
2021-03-20 13:24:56 +0100

    Adding Int64 cast

--- Sources/MIOCore/MIOCoreInt.swift
+++ Sources/MIOCore/MIOCoreInt.swift
@@ -22,6 +22,20 @@ public func MIOCoreIntValue ( _ value: Any?, _ def_value: Int? = nil ) -> Int? {
 }
 
 
+public func MIOCoreInt64Value ( _ value: Any?, _ def_value: Int64? = nil ) -> Int64? {
+    if let asString = value as? String { return Int64(asString) }
+    if let asBool   = value as? Bool   { return Int64(asBool ? 1 : 0 ) }
+    if let asInt    = value as? Int8   { return Int64(asInt) }
+    if let asInt    = value as? Int16  { return Int64(asInt) }
+    if let asInt    = value as? Int32  { return Int64(asInt) }
+    if let asInt    = value as? Int64  { return       asInt) }
+    if let asInt    = value as? Int    { return Int64(asInt) }
+    if let asDouble = value as? Double { return Int64(asDouble) }
+
+    return def_value
+}
+
+
 public func MIOCoreIsIntValue ( _ value: Any? ) -> Bool {
     if MIOCoreIntValue(value) == nil { return false }
     return true
