9afbf4b4c1d14b94ac0ddbf655030ca8b76057f0
Javier Segura <javier@miolabs.com>
2026-03-23 16:57:39 +0100

    adding nonisolated properties to url data task

--- Sources/MIOCore/Networking/URLSession+Extension.swift
+++ Sources/MIOCore/Networking/URLSession+Extension.swift
@@ -17,9 +17,9 @@ import FoundationNetworking
 extension URLSession
 {
     nonisolated public func synchronousDataTask(with request: URLRequest) -> (Data?, URLResponse?, Error?) {
-        var data: Data?
-        var response: URLResponse?
-        var error: Error?
+        nonisolated(unsafe) var data: Data?
+        nonisolated(unsafe) var response: URLResponse?
+        nonisolated(unsafe) var error: Error?
 
         let semaphore = DispatchSemaphore(value: 0)
 
@@ -38,14 +38,14 @@ extension URLSession
     }
     
     public func synchronousUploadTask(with request: URLRequest, data:Data?) -> (Data?, URLResponse?, Error?) {
-        var data: Data?
-        var response: URLResponse?
-        var error: Error?
+        nonisolated(unsafe) var resultData: Data?
+        nonisolated(unsafe) var response: URLResponse?
+        nonisolated(unsafe) var error: Error?
 
         let semaphore = DispatchSemaphore(value: 0)
 
         let dataTask = self.uploadTask(with: request, from: data) {
-            data = $0
+            resultData = $0
             response = $1
             error = $2
 
@@ -55,7 +55,7 @@ extension URLSession
 
         _ = semaphore.wait(timeout: .distantFuture)
 
-        return (data, response, error)
+        return (resultData, response, error)
     }
     
     
